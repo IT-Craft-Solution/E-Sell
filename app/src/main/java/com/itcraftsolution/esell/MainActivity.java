74dd3c6006f6b263719cc948f725530c6c01e253
Shadow <prathviksankaliya@gmail.com>
2022-01-10 19:41:55 +0530

    All Fragments Added And set Bottom Navigation Menu in Main Activity.

--- app/src/main/java/com/itcraftsolution/esell/MainActivity.java
+++ app/src/main/java/com/itcraftsolution/esell/MainActivity.java
@@ -1,17 +1,66 @@
 package com.itcraftsolution.esell;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationBarView;
+import com.itcraftsolution.esell.Fragment.AccountFragment;
+import com.itcraftsolution.esell.Fragment.ChatFragment;
+import com.itcraftsolution.esell.Fragment.HomeFragment;
+import com.itcraftsolution.esell.Fragment.MyAddFragment;
+import com.itcraftsolution.esell.Fragment.SellFragment;
+import com.itcraftsolution.esell.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+  private  ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        defView();
+        binding.mainBottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId())
+                {
+                    case R.id.bNavHome:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.bNavChat:
+                        selectedFragment = new ChatFragment();
+                        break;
+                    case R.id.bNavSell:
+                        selectedFragment = new SellFragment();
+                        break;
+                    case R.id.bNavMyAds:
+                        selectedFragment = new MyAddFragment();
+                        break;
+                    case R.id.bNavAccount:
+                        selectedFragment = new AccountFragment();
+                        break;
+
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frMainContainer , selectedFragment).commit();
+                return  true;
+            }
+        });
 
 
+    }
 
+    private void defView() {
+          FragmentTransaction firstFragment = getSupportFragmentManager().beginTransaction();
+            firstFragment.replace(R.id.frMainContainer, new HomeFragment());
+            firstFragment.commit();
     }
 }
