27ee903a5c970ce55cecad2026572a1be1208353
kirandhiyad <dhiyadkiran07@gmail.com>
2022-01-12 17:24:10 +0530

    Login Screen UI

--- app/src/main/java/com/itcraftsolution/esell/UserLogin.java
+++ app/src/main/java/com/itcraftsolution/esell/UserLogin.java
@@ -1,16 +1,34 @@
 package com.itcraftsolution.esell;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.itcraftsolution.esell.Fragments.login;
 
 public class UserLogin extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
 
-//        new chages for karan karan
+        getSupportActionBar().hide();
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frUserDetailsContainer , new login());
+        fragmentTransaction.commit();
+
     }
 }
+
