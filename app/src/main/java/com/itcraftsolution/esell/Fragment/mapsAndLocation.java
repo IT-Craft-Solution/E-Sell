4ac3421cc81704c937d462b6db59fcf340b6ea69
Shadow <prathviksankaliya@gmail.com>
2022-01-28 01:23:54 +0530

    Read web Api Atteched.

--- app/src/main/java/com/itcraftsolution/esell/Fragment/mapsAndLocation.java
+++ app/src/main/java/com/itcraftsolution/esell/Fragment/mapsAndLocation.java
@@ -1,22 +1,43 @@
 package com.itcraftsolution.esell.Fragment;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.itcraftsolution.esell.databinding.FragmentMapsAndLocationBinding;
 import com.itcraftsolution.esell.spf.SpfLoginUserData;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
 
 
 public class mapsAndLocation extends Fragment {
@@ -33,6 +54,7 @@ public class mapsAndLocation extends Fragment {
     private int  UserStatus;
     private GoogleSignInAccount account;
     private  SpfLoginUserData spfLoginUserData;
+    Uri uri;
 
 
     @Override
@@ -45,21 +67,70 @@ public class mapsAndLocation extends Fragment {
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
+        binding.ivNavigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                mGetContent.launch(intent);
+            }
+        });
         binding.btnLocationNavigation.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
 
-                if(GetUserData())
-                {
-                    Toast.makeText(requireContext(), "User Created", Toast.LENGTH_SHORT).show();
-//                    Intent intent = new Intent(getContext(),MainActivity.class);
-//                    startActivity(intent);
-//                    requireActivity().finishAffinity();
-                }
-                else {
-                    Toast.makeText(requireContext(), "Something went wrong!!", Toast.LENGTH_SHORT).show();
-                }
+                String url = "http://192.168.0.102:80/poetryapi/readpoetry.php";
+                RequestQueue requestQueue ;
+                requestQueue = Volley.newRequestQueue(requireContext());
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d("changeapp", response);
+                        binding.ivNavigation.setImageURI(Uri.parse("content://media/external_primary/images/media/1032218"));
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Log.d("changeapp", error.getMessage());
+                    }
+                });
+//                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://192.168.0.102:80/poetryapi/readpoetry.php", new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                Log.d("changeapp" , "responce is : "+response);
+//                Toast.makeText(requireContext(), ""+response, Toast.LENGTH_SHORT).show();
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//                Log.d("changeapp" , "Somthing went wrong");
+//                Log.d("changeapp", error.getMessage());
+//                Toast.makeText(requireContext(), ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        requestQueue.add(stringRequest);
+
+
+
+
+
+
+
+//                if(GetUserData())
+//                {
+//                    Toast.makeText(requireContext(), "User Created", Toast.LENGTH_SHORT).show();
+////                    Intent intent = new Intent(getContext(),MainActivity.class);
+////                    startActivity(intent);
+////                    requireActivity().finishAffinity();
+//                }
+//                else {
+//                    Toast.makeText(requireContext(), "Something went wrong!!", Toast.LENGTH_SHORT).show();
+//                }
 
             }
         });
@@ -67,6 +138,7 @@ public class mapsAndLocation extends Fragment {
 
         return binding.getRoot();
     }
+
     //Format Of DataBase Keys: phone,user_email,user_img,user_name,user_bio,user_location,user_area,user_status.
     private boolean GetUserData()
     {
@@ -96,6 +168,22 @@ public class mapsAndLocation extends Fragment {
 
         return true;
     }
+    ActivityResultLauncher<Intent> mGetContent = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK)
+                    {
+                        if(result.getData() != null)
+                        {
+                            uri = result.getData().getData();
+                            binding.ivNavigation.setImageURI(uri);
+                            Toast.makeText(requireContext(), ""+uri, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                }
+            });
 
 
 }
